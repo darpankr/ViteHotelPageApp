1fffc3865ddd8128f23298fa41c3eb20b2c9a870
darpankr <darpankumarsingworld42@gmail.com>
2025-06-03 12:04:53 +0530

    COmmit - Recomend component

--- src/components/RecomendHotels.jsx
+++ src/components/RecomendHotels.jsx
@@ -28,7 +28,7 @@ const RecomendHotels = () => {
                             </div>
                         </div>  
                     </div>
-                    <div class="col-span-3 flex justify-between items-center">
+                    <div class="col-span-3 flex flex-wrap justify-between items-center">
                         {/* <!-- price details --> */}
                         <div>
                             <div class="flex justify-start items-center gap-2">
@@ -46,7 +46,7 @@ const RecomendHotels = () => {
                         </div>
                     </div>
                 </div>
-                <div class="rounded border bg-white p-3 grid grid-cols-3 gap-2">
+                <div class="rounded border bg-gray-50 p-3 grid grid-cols-3 gap-2">
                     <div class="col-span-1">
                         <img class="rounded" src="https://images.unsplash.com/photo-1611892440504-42a792e24d32?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGhvdGVsfGVufDB8fDB8fHww" alt="suggect 1"/>
                     </div>
@@ -55,15 +55,15 @@ const RecomendHotels = () => {
                             <h1 class=" font-semibold">Hotel O Odumpur</h1>
                             <p class="text-sm text-gray-600">udhampur, Patanam . 4.1 km</p>
                         </div>
-                        <div>
-                            <div class="flex gap-2">
+                        <div className='flex flex-col justify-center items-start gap-2'>
+                            <div class="flex flex-wrap gap-0">
                                 <div class="bg-green-500 w-8 flex justify-center items-center rounded">
                                     <p class="text-white">4.2</p>
                                 </div>
                                 <p class="text-sm text-gray-500">(2 Ratings)</p>
                                 <p class="text-sm text-gray-500">Very Good</p>
                             </div>
-                            <div class="flex gap-2">
+                            <div class="flex flex-row flex-wrap gap-0">
                                 <p class="text-gray-700 text-sm">Free Wifi</p>
                                 <p class="text-gray-700 text-sm">Geysur</p>
                                 <p class="text-gray-700 text-sm">Power Backup</p>
@@ -71,7 +71,7 @@ const RecomendHotels = () => {
                             </div>
                         </div>  
                     </div>
-                    <div class="col-span-3 flex justify-between items-center">
+                    <div class="col-span-3 flex flex-wrap justify-between items-center">
                         {/* <!-- price details --> */}
                         <div>
                             <div class="flex justify-start items-center gap-2">
